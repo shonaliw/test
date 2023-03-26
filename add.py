6f07b4217d332fe7619082321096875a1e13c78f
Sanjeev <52065201+shonaliw@users.noreply.github.com>
2023-03-26 14:45:10 +0530

    Update add.py from Test2

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
-def add(a=1, b=2):
+def add(a=3, b=2):
 
-    """Returns the sum of a & b. Default Values for the parameters a is 1 & b is 2"""
+    """Returns the sum of a & b. Default Values for the parameters a is 3 & b is 2"""
 
     return a + b
 
